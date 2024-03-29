29e811d04c17c7c99e77ad5bb6622efc054892a4
Mica <25252229+MicaLovesKPOP@users.noreply.github.com>
2024-03-30 00:26:49 +0100

    Update ReplayLogger.js

--- ReplayLogger.js
+++ ReplayLogger.js
@@ -18,268 +18,136 @@ fs.readFile(usernameFilePath, 'utf8', (err, data) => {
     }
     const username = data.trim();
     replayFilePath = `C:\\Users\\${username}\\AppData\\Local\\Crashday\\replays\\_!lastracereplay!_.rpl`;
-console.log(`username.ini file path: ${usernameFilePath}`);
-console.log(`Replay file path: ${replayFilePath}`);
+    console.log(`username.ini file path: ${usernameFilePath}`);
+    console.log(`Replay file path: ${replayFilePath}`);
 });
 
-      
+let latestLogFilePath = null;
+
 const interval = setInterval(() => {
-  if (fs.existsSync(replayFilePath)) {
-    try {
-      // Get the modification date of the replay file
-      const replayFileStats = fs.statSync(replayFilePath);
-      const replayFileModDate = replayFileStats.mtime;
-      
-      // Create the output directory
-      const outputDirName = replayFileModDate.toISOString().slice(0, 10);
-      const outputDirPath = path.join(path.dirname(replayFilePath), outputDirName);
-      if (!fs.existsSync(outputDirPath)) {
-        fs.mkdirSync(outputDirPath);
-      }
-      
-      // Replace the desired output file name
-      const eventNumber = fs.readdirSync(outputDirPath).length + 1;
-      const gamemodeMatch = fs.readFileSync(replayFilePath, 'utf8').match(/gamevar=(\S+)/);
-      const gamemodeValue = gamemodeMatch ? gamemodeMatch[1] : 'unknown';
-      const outputFileName = `${outputDirName} ${getFormattedTime(replayFileModDate)} event${eventNumber} ${gamemodeValue}.log`;
-      const outputFilePath = path.join(outputDirPath, outputFileName);
+    if (fs.existsSync(replayFilePath)) {
+        try {
+            // Get the modification date of the replay file
+            const replayFileStats = fs.statSync(replayFilePath);
+            const replayFileModDate = replayFileStats.mtime;
 
-// Read the .rpl file
-fs.readFile(replayFilePath, 'utf8', (err, data) => {
-    if (err) {
-        console.error(err);
-        return;
-    }
+            // Create the output directory
+            const outputDirName = replayFileModDate.toISOString().slice(0, 10);
+            const outputDirPath = path.join(path.dirname(replayFilePath), outputDirName);
+            if (!fs.existsSync(outputDirPath)) {
+                fs.mkdirSync(outputDirPath);
+            }
 
-    // Ignore the initial characters in the file
-    const startIndex = data.indexOf('RP#');
-    const trimmedTrackData = data.substring(startIndex);
+            // Find the next available sequential number for file names
+            let nextNumber = 1;
+            while (fs.existsSync(path.join(outputDirPath, `event${nextNumber}.log`))) {
+                nextNumber++;
+            }
 
-    // Extract the track value
-    const trackMatch = trimmedTrackData.match(/([0-9`~!@#$%^&()_+\-=[\]{};,. a-z]+?\.trk)/);
-    // console.log('trackMatch:', trackMatch);
-    const trackValue = trackMatch ? trackMatch[1] : 'unknown';
+            // Replace the desired output file name
+            const gamemodeMatch = fs.readFileSync(replayFilePath, 'utf8').match(/gamevar=(\S+)/);
+            const gamemodeValue = gamemodeMatch ? gamemodeMatch[1] : 'unknown';
+            const outputFileName = `event${nextNumber} ${getFormattedTime(replayFileModDate)} ${gamemodeValue}.log`;
+            latestLogFilePath = path.join(outputDirPath, outputFileName);
 
-    // Ignore the initial characters in the file
-    const endIndex = data.indexOf('\0', startIndex);
-    let trimmedData = trimmedTrackData.substring(startIndex);
+            // Read the .rpl file
+            fs.readFile(replayFilePath, 'utf8', (err, data) => {
+                if (err) {
+                    console.error(err);
+                    return;
+                }
 
-    // Remove blocks of null characters that are over 10 characters in size from trimmedData
-    trimmedData = trimmedData.replace(/\0{10,}/g, '');
-    console.log('trimmedData:', trimmedData.substring(0,2048));
+                // Process replay file data and extract relevant information
 
-        // Extract the replay event details
-        const replayEventDetails = `Replay Event Details: ${outputFileName}\n\n`;
-      
-        // Extract the gamemode value
-        const gamemodeValue = gamemodeMatch ? gamemodeMatch[1] : 'unknown';
-      
-        // Extract the gamemode values
-        const gamemodeMatches = trimmedData.match(/gamemode\w+=(\d+)/g);
-        const gamemodeValues = gamemodeMatches ? gamemodeMatches.map(match => match.split('=')[1]) : [];
-      
-        // Determine the gamemode value to display
-        let gamemodeValueToDisplay = '';
-        if (gamemodeValues.length === 0) {
-          gamemodeValueToDisplay = 'unknown';
-        } else if (gamemodeValues.every(value => value === 'unknown')) {
-          gamemodeValueToDisplay = 'unknown';
-        } else {
-          gamemodeValueToDisplay = gamemodeValues.join(', ');
-        }
-      
-        // Format the extracted values
-        const gamemode = `Gamemode: ${gamemodeValue}, ${gamemodeValueToDisplay}\n`;
-        const trackName = `Track: ${trackValue}\n\n`;
-      
-        // Extract the remaining non-player data
-        const damageMatch = trimmedData.match(/damagerealism=(-?\d+)/);
-        let damageValue;
-        if (damageMatch) {
-          switch (damageMatch[1]) {
-            case '-1':
-              damageValue = 'Disabled';
-              break;
-            case '0':
-              damageValue = 'Low';
-              break;
-            case '1':
-              damageValue = 'Medium';
-              break;
-            case '2':
-              damageValue = 'High';
-              break;
-            default:
-              damageValue = 'unknown';
-          }
-        } else {
-          damageValue = 'unknown';
-        }
-      
-        const missilesMatch = trimmedData.match(/forcemissile\[\*\]=(\S+)/);
-        let missilesValue;
-        if (missilesMatch) {
-          switch (missilesMatch[1]) {
-            case 'none':
-              missilesValue = 'Disabled';
-              break;
-            case 'single':
-              missilesValue = 'Enabled, single';
-              break;
-            case 'double':
-              missilesValue = 'Enabled, double';
-              break;
-            default:
-              missilesValue = 'unknown';
-          }
-        } else {
-          missilesValue = 'unknown';
-        }
-      
-        const minigunMatch = trimmedData.match(/forceminigun\[\*\]=(\S+)/);
-        let minigunValue;
-        if (minigunMatch) {
-          switch (minigunMatch[1]) {
-            case 'none':
-              minigunValue = 'Disabled';
-              break;
-            case 'force':
-              minigunValue = 'Enabled, single';
-              break;
-            case 'forcedouble':
-              minigunValue = 'Enabled, double';
-              break;
-            default:
-              minigunValue = 'unknown';
-          }
-        } else {
-          minigunValue = 'unknown';
-        }
-      
-        const pickupsMatch = trimmedData.match(/forcepickups=(\d+)/);
-        let pickupsValue;
-        if (pickupsMatch) {
-          switch (pickupsMatch[1]) {
-            case '0':
-              pickupsValue = 'Disabled';
-              break;
-            case '1':
-              pickupsValue = 'Enabled';
-              break;
-            default:
-              pickupsValue = 'unknown';
-          }
-        } else {
-          pickupsValue = 'unknown';
-        }
-      
-        const nitroMatch = trimmedData.match(/forcenitro\[\*\]=(\S+)/);
-        let nitroValue;
-        if (nitroMatch) {
-          switch (nitroMatch[1]) {
-            case 'abnitro':
-              nitroValue = 'Enabled';
-              break;
-            case 'none':
-              nitroValue = 'Disabled';
-              break;
-            default:
-              nitroValue = 'unknown';
-          }
-        } else {
-          nitroValue = 'unknown';
-        }
-      
-        // Extract the timeofday value
-        const timeofdayMatch = trimmedData.match(/forcetimeofday=(\S+)/);
-        const timeofdayValue = timeofdayMatch ? timeofdayMatch[1] : '';
-      
-        // Format the extracted value
-        const timeofday = timeofdayValue ? `Ambience: ${timeofdayValue}\n` : '';
-        const damage = `Damage: ${damageValue}\n`;
-        const missiles = `Missiles: ${missilesValue}\n`;
-        const minigun = `Minigun: ${minigunValue}\n\n`;
-      
-        const pickups = `Pickups: ${pickupsValue}\n`;
-        const nitro = `Nitro: ${nitroValue}\n`;
-      
-        // Extract the additional data
-        let additionalData = '';
-        
-        const additionalDataRegex = /(force\w+)\[(\d+|\*)\]=(\S+)/g;
-        let match;
-        while ((match = additionalDataRegex.exec(trimmedData)) !== null) {
-          const [_, key, index, value] = match;
-          if (!index.startsWith('76561') && key !== 'forcemines' && key !== 'forceaiskill' && key !== 'gamevar' && key !== 'gamemodevalue' && key !== 'timeofday' && key !== 'damagerealism' && key !== 'forcemissile' && key !== 'forceminigun' && key !== 'forcepickups' && key !== 'forcenitro') {
-            additionalData += `${key}[${index}]: ${value}\n`;
-          }
-        }
-        additionalData += '\n';
-      
-        // Extract the player data
-        const playerData = extractPlayerData(trimmedData);
-      
-        // Format the players line
-        let players;
-        if (playerData && playerData.length > 0) {
-          players = `Players (${playerData.length}):`;
-        } else {
-          players = 'Players (unknown):';
-        }
-      
-        // Format the player list
-        let playerList = '';
-        if (playerData && playerData.length > 0) {
-          for (const player of playerData) {
-            const playerName = player.name.replace(/\\\d/g, '');
-            const playerInfo = `\n- ${playerName}\n  Car: ${player.car}\n  Car Color: ${player.carcolor}\n  Cartunings: ${player.cartunings}\n  Wheels: ${player.wheel}\n  Analog Steering: ${player.analogsteering}\n  Steering Speed: ${player.steeringspeed}\n`; // excluded: Nitro Color: ${player.nitrocolor}\n  
-            playerList += playerInfo;
-          }
-        } else {
-          playerList += ' unknown';
-        }
-      
-        // Combine the players line and player list
-        const playersAndList = `${players}${playerList}`;
-      
-        // Combine all the extracted data
-        const replayDetails = replayEventDetails + gamemode + trackName + damage + missiles + minigun + pickups + nitro + timeofday + additionalData + playersAndList;
-      
-        // Write the data to the output file
-        fs.writeFile(outputFilePath, replayDetails, 'utf8', (err) => {
-          if (err) {
-            console.error(err);
+                // Write the data to the latest log file
+                fs.writeFile(latestLogFilePath, replayDetails, 'utf8', (err) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+
+                    console.log(`Latest replay details saved to ${outputFileName}`);
+
+                    // Remove older log files
+                    removeOlderLogFiles(outputDirPath, latestLogFilePath);
+
+                    // Update file names to remove gaps and ensure sequential numbering
+                    updateFileNames(outputDirPath);
+                });
+            });
+
+            // Helper function to remove older log files
+            function removeOlderLogFiles(outputDirPath, latestLogFilePath) {
+                fs.readdir(outputDirPath, (err, files) => {
+                    if (err) {
+                        console.error(err);
+                        return;
+                    }
+                    files.forEach((file) => {
+                        if (file !== latestLogFilePath.split(path.sep).pop()) {
+                            const filePath = path.join(outputDirPath, file);
+                            fs.unlink(filePath, (err) => {
+                                if (err) {
+                                    console.error(`Error deleting file ${filePath}:`, err);
+                                } else {
+                                    console.log(`Deleted file: ${filePath}`);
+                                }
+                            });
+                        }
+                    });
+                });
+            }
+
+// Function to update file names to remove gaps and ensure sequential numbering
+function updateFileNames(folderPath) {
+    fs.readdir(folderPath, (err, files) => {
+        if (err) {
+            console.error(`Error reading folder ${folderPath}:`, err);
             return;
-          }
-      
-          console.log(`Replay details saved to ${outputFileName}`);
-        });
-      });
-      
-      // Helper function to get the formatted time
-      function getFormattedTime(date) {
-        return date.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' }).replace(/:/g, '.');
-      }
-      
-      // Helper function to extract the player data
-      function extractPlayerData(data) {
-        const playerData = [];
-        const playerRegex = /forcecar\[(\d+)\]=(\S+) forcesteeringspeed\[\1\]=(\S+) forceanalogsteer\[\1\]=(\S+) forcedrivername\[\1\]=(.+?) forcecarcolor\[\1\]=(\S+) forcenitrocolor\[\1\]=(\S+) forcecartunings\[\1\]=(.+?) forcewheel\[\1\]=(.+?) /g;
-        let match;
-        while ((match = playerRegex.exec(data)) !== null) {
-          const [_, id, car, steeringspeed, analogsteering, name, carcolor, nitrocolor, cartunings, wheel] = match;
-          const playerName = name.replace(/\\./g, '').replace(/\|/g, ' ');
-          playerData.push({ id, car, steeringspeed, analogsteering, name: playerName, carcolor, nitrocolor, cartunings, wheel });
         }
-        return playerData.length > 0 ? playerData : null;
-      }
 
-	  // Move the replay file to the output directory
-	  fs.renameSync(replayFilePath, path.join(outputDirPath, outputFileName.replace('.log', '.rpl')));
-    } catch (err) {
-      console.error(err);
+        // Extract existing sequential numbers from file names with .log extension
+        const existingNumbers = files
+            .filter((file) => file.endsWith('.log'))
+            .map((file) => {
+                const match = file.match(/^event(\d+)\.log$/);
+                return match ? parseInt(match[1]) : null;
+            })
+            .filter((number) => number !== null);
+
+        // Find the maximum existing sequential number
+        const maxNumber = Math.max(...existingNumbers);
+
+        // Find the next available sequential number
+        const nextNumber = maxNumber !== -Infinity ? maxNumber + 1 : 1;
+
+        files.forEach((file) => {
+            const expectedName = `event${nextNumber}.log`;
+            if (file !== expectedName) {
+                const oldFilePath = path.join(folderPath, file);
+                const newFilePath = path.join(folderPath, expectedName);
+                fs.rename(oldFilePath, newFilePath, (err) => {
+                    if (err) {
+                        console.error(`Error renaming file ${oldFilePath} to ${newFilePath}:`, err);
+                    } else {
+                        console.log(`Renamed file: ${oldFilePath} to ${newFilePath}`);
+                    }
+                });
+            }
+        });
+    });
+}
+
+			
+        } catch (err) {
+            console.error(err);
+        }
+    } else {
+        console.log('Replay file not found');
     }
-  } else {
-    console.log('Replay file not found');
-  }
 }, 10000);
+
+// Helper function to get the formatted time
+function getFormattedTime(date) {
+    return date.toLocaleTimeString('en-US', { hour12: false, hour: '2-digit', minute: '2-digit' }).replace(/:/g, '.');
+}
